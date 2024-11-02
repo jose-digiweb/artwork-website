aa7322ce29bc3223100ac78fae4374e137e8b1f8
jose-digiweb <josefurtado.digital@gmail.com>
2024-11-02 18:54:16 +0100

    minor change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
-import { Button } from '@/components/ui/button';
-
 export default function Home() {
   return (
     <div className='flex flex-col w-full h-full font-[family-name:var(--font-geist-sans)]'>
-      <main className='flex flex-col w-full h-full max-w-screen-lg mx-auto'>
-        <div className='w-full flex flex-col'>
-          <span>BangeYhodhy Abstract Painting</span>
-          <Button>View All Artworks</Button>
-        </div>
+      <main className='flex flex-col gap-2 w-full h-full p-4 sm:p-8 max-w-screen-lg mx-auto'>
+        <h1 className='text-5xl font-bold'>BangeYhodhy</h1>
+
+        <p className='text-pretty max-w-lg'>
+          Discover stunning abstract paintings, by BangeYhodhy, a talented artist from
+          Guinea Bissau, based in Rotterdam. Each piece is unique, and each one is a work
+          of art.
+        </p>
       </main>
     </div>
   );
