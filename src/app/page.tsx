c1d0a80fbf152af458b47fb19b143f2019ef5012
jose-digiweb <josefurtado.digital@gmail.com>
2024-11-02 18:40:17 +0100

    minor change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,20 +4,10 @@ export default function Home() {
   return (
     <div className='flex flex-col w-full h-full font-[family-name:var(--font-geist-sans)]'>
       <main className='flex flex-col w-full h-full max-w-screen-lg mx-auto'>
-        <div className='w-full flex items-center justify-between'>
-          <span className='text-5xl font-bold'>BangeYhodhy</span>
-          <span className='text-3xl font-bold'>Abstract Painting</span>
-        </div>
-
         <div className='w-full flex flex-col'>
-          <span className='text-2xl font-bold'>
-            Discover stunning abstract paintings, by BangeYhodhy, a talented artist from
-            Guinea Bissau, based in Rotterdam. Each piece is unique, and each one is a
-            work of art.
-          </span>
+          <span>BangeYhodhy Abstract Painting</span>
+          <Button>View All Artworks</Button>
         </div>
-
-        <Button>View All Artworks</Button>
       </main>
     </div>
   );
