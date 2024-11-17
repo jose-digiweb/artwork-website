b8055fcadf805c133c60187e5b8f55a8c368e155
jose-digiweb <josefurtado.digital@gmail.com>
2024-11-17 15:51:42 +0100

    added fav icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,6 @@ const geistMono = localFont({
   variable: '--font-geist-mono',
   weight: '100 900',
 });
-
 const kalam = Kaushan_Script({
   subsets: ['latin'],
   weight: ['400'],
@@ -34,6 +33,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en' className='font-[family-name:var(--font-geist-sans)]'>
+      <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
+      <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
+      <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
+      <link rel='manifest' href='/site.webmanifest' />
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${kalam.variable} antialiased w-full h-dvh overflow-hidden bg-gradient-to-br from-rose-100 via-fuchsia-100 to-indigo-100`}
       >
