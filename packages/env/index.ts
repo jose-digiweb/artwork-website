37ce28d9ade898e73260e43c2b6f1854cbb9d6fb
jose-digiweb <josefurtado.digital@gmail.com>
2024-12-30 19:45:09 +0100

    testing env

--- packages/env/index.ts
+++ packages/env/index.ts
@@ -3,25 +3,14 @@ import { z } from 'zod';
 
 const server: Parameters<typeof createEnv>[0]['server'] = {
   DATABASE_URL: z.string().min(1).url(),
-
-  // Added by Vercel
-  VERCEL: z.string().optional(),
-  NEXT_RUNTIME: z.enum(['nodejs', 'edge']).optional(),
 };
 
-const client: Parameters<typeof createEnv>[0]['client'] = {
-  // Added by Vercel
-  NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL: z.string().min(1).url(),
-};
+const client: Parameters<typeof createEnv>[0]['client'] = {};
 
 export const env = createEnv({
   client,
   server,
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
-    VERCEL: process.env.VERCEL,
-    NEXT_RUNTIME: process.env.NEXT_RUNTIME,
-    NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL:
-      process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL,
   },
 });
