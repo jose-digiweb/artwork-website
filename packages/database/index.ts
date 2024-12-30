e4969addc10f216a21448548c85c7178d00a646f
jose-digiweb <josefurtado.digital@gmail.com>
2024-12-30 19:12:30 +0100

    Extracted artwork type

--- packages/database/index.ts
+++ packages/database/index.ts
@@ -2,11 +2,27 @@ import 'server-only';
 
 import { Pool, neonConfig } from '@neondatabase/serverless';
 import { PrismaNeon } from '@prisma/adapter-neon';
-import { PrismaClient, type Artwork } from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
 import { env } from '@bangeyhodhy/env';
 import ws from 'ws';
 
 neonConfig.webSocketConstructor = ws;
+
+export type Artwork = {
+  description: string;
+  id: string;
+  title: string;
+  dimensions: string;
+  size: string;
+  imageUrl: string;
+  thumbnailUrl: string;
+  imageSecureUrl: string;
+  imagePublicId: string;
+  year: string;
+  featured: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+};
 
 declare global {
   var cachedPrisma: PrismaClient | undefined;
@@ -16,5 +32,3 @@ const pool = new Pool({ connectionString: env.DATABASE_URL });
 const adapter = new PrismaNeon(pool);
 
 export const database = new PrismaClient({ adapter });
-
-export type { Artwork };
